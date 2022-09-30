cd1f876f7a89a5c79e5cf4f3dbe4c4d9da2a42d1
kuznetsov.n <nikita.kuznetsov.r@gmail.com>
2022-09-30 20:11:35 +0300

    стили калькулятора

--- src/components/Calculator/Calculator.vue
+++ src/components/Calculator/Calculator.vue
@@ -1,60 +1,63 @@
 <template>
     <div class="calculator">
-        <div class="calculator__item">
-            <div class="calculator-field">
-                <p class="calculator__name">Стоимость автомобиля</p>
-                <div class="calculator-field__box p-inputgroup">
-                    <InputText
-                        class="calculator-field__input"
-                        placeholder="3 300 000"
-                    />
-                    <span class="calculator-field__info p-inputgroup-addon"
-                        >₽</span
-                    >
+        <div class="calculator__body">
+            <div class="calculator__item">
+                <div class="calculator-field">
+                    <p class="calculator__name">Стоимость автомобиля</p>
+                    <div class="calculator-field__box p-inputgroup">
+                        <InputText
+                            class="calculator-field__input"
+                            placeholder="3 300 000"
+                        />
+                        <span class="calculator-field__info p-inputgroup-addon"
+                            >₽</span
+                        >
+                    </div>
+                    <Slider v-model="value" class="calculator-field__range" />
                 </div>
-                <Slider />
             </div>
-        </div>
-        <div class="calculator__item">
-            <div class="calculator-field">
-                <p class="calculator__name">Первоначальный взнос</p>
-                <div class="calculator-field__box p-inputgroup">
-                    <InputText
-                        class="calculator-field__input"
-                        placeholder="420 000 ₽"
-                    />
-                    <span class="calculator-field__info p-inputgroup-addon"
-                        >13%</span
-                    >
+            <div class="calculator__item">
+                <div class="calculator-field">
+                    <p class="calculator__name">Первоначальный взнос</p>
+                    <div class="calculator-field__box p-inputgroup">
+                        <InputText
+                            class="calculator-field__input"
+                            placeholder="420 000 ₽"
+                        />
+                        <span
+                            class="calculator-field__info calculator-field__info--percent p-inputgroup-addon"
+                            ><span>13%</span></span
+                        >
+                    </div>
+                    <Slider class="calculator-field__range" />
                 </div>
-                <Slider />
             </div>
-        </div>
-        <div class="calculator__item">
-            <div class="calculator-field">
-                <p class="calculator__name">Срок лизинга</p>
-                <div class="calculator-field__box p-inputgroup">
-                    <InputText
-                        class="calculator-field__input"
-                        placeholder="60"
-                    />
-                    <span class="calculator-field__info p-inputgroup-addon"
-                        >мес.</span
-                    >
+            <div class="calculator__item">
+                <div class="calculator-field">
+                    <p class="calculator__name">Срок лизинга</p>
+                    <div class="calculator-field__box p-inputgroup">
+                        <InputText
+                            class="calculator-field__input"
+                            placeholder="60"
+                        />
+                        <span class="calculator-field__info p-inputgroup-addon"
+                            >мес.</span
+                        >
+                    </div>
+                    <Slider class="calculator-field__range" />
                 </div>
-                <Slider />
             </div>
-        </div>
-        <div class="calculator__item">
-            <p class="calculator__name">Сумма договора лизинга</p>
-            <div class="calculator__result">4 467 313 ₽</div>
-        </div>
-        <div class="calculator__item">
-            <p class="calculator__name">Ежемесячный платеж от</p>
-            <div class="calculator__result">4 467 313 ₽</div>
-        </div>
-        <div class="calculator__item">
-            <button class="calculator__btn">Оставить заявку</button>
+            <div class="calculator__item">
+                <p class="calculator__name">Сумма договора лизинга</p>
+                <div class="calculator__result">4 467 313 ₽</div>
+            </div>
+            <div class="calculator__item">
+                <p class="calculator__name">Ежемесячный платеж от</p>
+                <div class="calculator__result">4 467 313 ₽</div>
+            </div>
+            <div class="calculator__item">
+                <button class="calculator__btn">Оставить заявку</button>
+            </div>
         </div>
     </div>
 </template>
@@ -69,6 +72,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import 'styles/main';
 </style>
