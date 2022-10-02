48f394ca76f7ea5d99fbbf1f8eb7f18121ccdf38
kuznetsov.n <nikita.kuznetsov.r@gmail.com>
2022-10-02 14:02:38 +0300

    добавил прелоадер

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
     <div class="page">
+        <Transition name="fade">
+            <div v-if="preloader" class="preloader">
+                <div class="lds-roller">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </Transition>
         <PriceLeasing class="page__calculator"></PriceLeasing>
     </div>
 </template>
@@ -8,6 +22,16 @@
 import PriceLeasing from './components/PriceLeasing/PriceLeasing.vue'
 export default {
     components: { PriceLeasing },
+    data() {
+        return {
+            preloader: true,
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.preloader = false
+        }, 2000)
+    },
 }
 </script>
 
